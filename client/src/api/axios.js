0fc321354e3db551b35ec7d2b9bb9ed521b63cbc
Usman Dayyabu <usmandayyabu17@gmail.com>
2025-12-15 18:26:10 +0100

    Refactor axios base URL configuration and add .env.production file

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
+// This logic checks: "Are we on the live site?"
+// If YES -> Use the Render link.
+// If NO  -> Use the Localhost link.
+const baseUrl = import.meta.env.MODE === 'production' 
+  ? 'https://nexus-c2v7.onrender.com/api' 
+  : 'http://localhost:5000/api';
+
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Pointing to our Express Server
+  baseURL: baseUrl, 
 });
 
 export default api;
