38fb0c6f480c89aebe5c275f4f04b4f1cb074b6c
Usman Dayyabu <usmandayyabu17@gmail.com>
2026-01-06 21:59:25 +0100

    Binds server to all network interfaces
    Allows external devices to access the server by listening on 0.0.0.0 instead of the default localhost. Useful for deployment in containerized or cloud environments.

--- server/index.js
+++ server/index.js
@@ -85,6 +85,6 @@ connectDB(); // Call the connectDB function BEFORE starting the server
 const PORT = process.env.PORT || 5000;
 
 // Start Server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
